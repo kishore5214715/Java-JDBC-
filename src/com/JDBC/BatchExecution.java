8679819a665c9a2469f4760775078d56590d7958
kishore5214715 <kishoresachin5214715@gmail.com>
2024-09-05 00:50:05 +0530

    Batch Program

---
+++ src/com/JDBC/BatchExecution.java
@@ -0,0 +1,34 @@
+package com.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class BatchExecution {
+    public static void main(String[] args) {
+        String sql = "insert into jet(id,name,age) values(?,?,?)";
+        try {
+            //to connect with the data base
+            Scanner sc = new Scanner(System.in);
+            Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "kishore");
+            //to modify with the db and use an preparedStatement
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            for(int i=0;i<2;i++) {
+                //get the input form hte user
+                preparedStatement.setInt(1,sc.nextInt());
+                preparedStatement.setString(2,sc.next());
+                preparedStatement.setInt(3,sc.nextInt());
+
+                //add to the batches
+                preparedStatement.addBatch();
+            }
+            //execute hte batches
+            preparedStatement.executeBatch();
+            System.out.println("Done");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
